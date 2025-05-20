a81bf478f63b14501e38e02c207cd4953b7712e0
pythedev <pyork@ihg.co.uk>
2025-05-20 21:03:17 +0100

    v3.4

--- modal-popup-plugin.php
+++ modal-popup-plugin.php
@@ -39,23 +39,56 @@ if (function_exists('acf_add_options_page')) {
 // Display Modal
 add_action('wp_footer', function() {
     if (!function_exists('get_field')) return;
-    if (!get_field('enable_modal', 'option')) return;
+   
 
     global $post;
-    $selected_pages = get_field('show_on_pages', 'option');
+     $global_enabled = get_field('enable_modal', 'option');
+    $global_pages = get_field('show_on_pages', 'option');
+    $modals = get_field('page_per_modals', 'option');
+ $global_content = get_field('popup_content', 'option');
+     $modal_content = '';
+    $bg_color = '#ffffff';
+    $overlay_color = 'rgba(0,0,0,0.6)';
+    $show = false;
 
-    if ($selected_pages && !in_array($post->ID, array_column($selected_pages, 'ID'))) return;
-    $modal_add_image = get_field('modal_add_image', 'option');
-    $modal_content = get_field('popup_content', 'option');
-    $modal_image = get_field('modal_image', 'option');
-    $modal_button = get_field('modal_button', 'option');
-    $pop_modal_width = get_field('pop_modal_width', 'option');
-    $modal_mobile_image_fit = get_field('modal_mobile_image_fit', 'option');
-    $bg_color = get_field('modal_bg_color', 'option') ?: '#ffffff';
-     $overlay_color = get_field('modal_overlay_color', 'option') ?: 'rgba(0,0,0,0.6)';
-    if ($modal_add_image && $modal_image) {
+     if ($modals) {
+        foreach ($modals as $modal) {
+            if ($modal['target_page']['ID'] == $post->ID) {
+                $modal_content = $modal['modal_content'];
+                $bg_color = $modal['modal_bg_color'] ?: $bg_color;
+                $overlay_color = $modal['modal_overlay_color'] ?: $overlay_color;
+                $pop_modal_width = $modal['pop_modal_width'];
+                $modal_image = $modal['modal_image'];
+                $modal_add_image = $modal['modal_add_image'];
+                 if ($modal_add_image && $modal_image) {
         $modal_img_content .= '<img src="' . esc_url($modal_image['url']) . '" class="modal-img" alt="' . esc_attr($modal_image['alt']) . '">';
     }
+                $show = true;
+                break;
+            }
+        }
+    }
+
+      if (!$show && $global_enabled && is_array($global_pages)) {
+        if (in_array($post->ID, array_column($global_pages, 'ID'))) {
+            $modal_content = $global_content;
+            $modal_add_image = get_field('modal_add_image', 'option');
+               $modal_image = get_field('modal_image', 'option');
+    $modal_button = get_field('modal_button', 'option');
+      $modal_mobile_image_fit = get_field('modal_mobile_image_fit', 'option');
+            $bg_color = get_field('modal_bg_color', 'option') ?: $bg_color;
+            $overlay_color = get_field('modal_overlay_color', 'option') ?: $overlay_color;
+             $pop_modal_width = get_field('pop_modal_width', 'option');
+            if ($modal_add_image && $modal_image) {
+        $modal_img_content .= '<img src="' . esc_url($modal_image['url']) . '" class="modal-img" alt="' . esc_attr($modal_image['alt']) . '">';
+    }
+            $show = true;
+        }
+    }
+
+    if (!$show || !$modal_content) return;
+
+ 
     ?>
      <style>
         .custom-modal { background-color: <?php echo $overlay_color; ?>; }
